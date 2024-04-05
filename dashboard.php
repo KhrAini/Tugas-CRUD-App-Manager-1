fc1ff34db201de7016358afd9ee8c5ddf170cbd2
Khurotul Aini <ayni110603@gmail.com>
2024-04-05 20:49:08 +0700

    first commit

---
+++ dashboard.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Reservation - Dashboard</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+    <div class="halaman2">
+        <div class="sidebar">
+            <h1><img src="icon.png" ></h1>
+            <h2>My Account</h2>
+            <ul>
+                <li>History</li>
+                <li>Setting</li>
+                <li>Help</li>
+            </ul>
+            <form>
+                <button type="submit">Logout</button>
+            </form>
+              
+        </div>
+    
+        <div class="dashboard" >
+            <h1>Restaurant Reservation</h1>
+    
+            <div class="reservation">
+                <h2>Reservations</h2>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tanggal Reservasi</th>
+                            <th>Untuk Tanggal</th>
+                            <th> Nama </th>
+                            <th>Nomor Telepon</th>
+                            <th>Nomor Meja</th>
+                            <th>Jumlah Tamu</th>
+                            <th>Status Pembayaran</th>                </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>12 April 2024</td>
+                            <td>14 April 2024</td>
+                            <td>Oliv</td>
+                            <td>08123456789</td>
+                            <td>5</td>
+                            <td>3</td>
+                            <td>Lunas</td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td>13 April 2024</td>
+                            <td>20 April 2024</td>
+                            <td>Mila</td>
+                            <td>08128756269</td>
+                            <td>12</td>
+                            <td>4</td>
+                            <td>Belum Bayar</td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                        </tr>
+                    </tbody>
+                    <tbody>
+                        <tr>
+                            <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                        </tr>
+                    </tbody>
+                                    <tbody>
+                        <tr>
+                            <td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <button>Edit</button>
+                <button>Delete</button>
+            </div>
+    
+        </div>
+    </div>
+
+
+
+
+</body>
+</html>
